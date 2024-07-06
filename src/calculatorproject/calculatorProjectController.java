3dc15a82efb7a3b82683789a11ea95dffcba0412
Burak_Ezeroglu_ <ezerogluburak@gmail.com>
2024-07-06 12:09:41 +0300

    shared

---
+++ src/calculatorproject/calculatorProjectController.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorproject;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author LENOVO
+ */
+public class calculatorProjectController implements Initializable {
+
+    @FXML
+    private TextField result;
+    @FXML
+    private Button seven;
+    @FXML
+    private Button eight;
+    @FXML
+    private Button nine;
+    @FXML
+    private Button four;
+    @FXML
+    private Button five;
+    @FXML
+    private Button six;
+    @FXML
+    private Button one;
+    @FXML
+    private Button two;
+    @FXML
+    private Button three;
+    @FXML
+    private Button equalsButton;
+    @FXML
+    private Button zero;
+    @FXML
+    private Button division;
+    @FXML
+    private Button multiply;
+    @FXML
+    private Button minus;
+    @FXML
+    private Button add;
+    @FXML
+    private Button deleteButton;
+    @FXML
+    private Button clearButton;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onSevenClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onEightClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onNineClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onFourClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onFiveClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onSixClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onOneClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onTwoClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onThreeClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onEqualsButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onZeroClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onDivisionButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onMultiplyButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onMinusButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onAddButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onDeleteButtonClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void onClearButtonClicked(ActionEvent event) {
+    }
+
+    
+}
